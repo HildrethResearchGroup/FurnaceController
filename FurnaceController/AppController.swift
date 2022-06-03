4d39a31c3c4d970f86c4921dd0961c7e6912e378
Josh Mendelsohn <joshmendels@gmail.com>
2022-06-03 10:33:30 -0600

    added comments

--- FurnaceController/AppController.swift
+++ FurnaceController/AppController.swift
@@ -9,31 +9,36 @@ import Foundation
 
 class AppController: ObservableObject {
     
+    // sub-controllers, used to manipulate data and graphs
     var arduino = ArduinoController()
     var graph = GraphController()
     
+    // timer functions
     @Published var recordButtonLabel: String = "Start Recording"
     @Published var progressTime: Int = 0
     @Published var minutesPerSample = "1"
     
+    // timers for the stopwatch and data polling
     var stopwatchTimer: Timer?
     var pollingTimer: Timer?
     
+    // tell arduino to get data
     func pollForData() {
         arduino.readTemperature()
         arduino.readArgonFlow()
         arduino.readNitrogenFlow()
-        print("polled")
     }
     
+    // starts and stops logic recording
     func startOrStopRecord(){
         if(self.recordButtonLabel == "Start Recording"){
             
             let minsPerSamp = Double(self.minutesPerSample)
-            
             self.recordButtonLabel = "Stop Recording"
-            self.progressTime = 0
             
+            self.progressTime = 0 // reset progress timer
+            
+            // initialize the timers
             stopwatchTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
                 self.progressTime = self.progressTime + 1
             }
@@ -42,8 +47,8 @@ class AppController: ObservableObject {
                 self.pollForData()
             }
             
+            // poll for data right away to get imediate data (and not have to wait for timer)
             self.pollForData()
-            
         }
         else{
             self.recordButtonLabel = "Start Recording"
