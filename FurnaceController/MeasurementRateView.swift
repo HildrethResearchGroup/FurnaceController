3a0f7fce804e7077a1e1689f7e03fa4dba3301ad
Preston Yates <pyates@mines.edu>
2022-06-07 11:18:23 -0600

    got Sampling rate view to look similar as other views

--- FurnaceController/MeasurementRateView.swift
+++ FurnaceController/MeasurementRateView.swift
@@ -10,15 +10,65 @@ import SwiftUI
 struct MeasurementRateView: View {
     
     @ObservedObject var controller: AppController
-    
+    @State private var rate: String = ""
     init(controller: AppController) {
         self.controller = controller
     }
     
+    
+    
+    var body: some View {
+        VStack {
+            
+            HStack {
+                
+                // MARK: Display Flowrate
+                // Displays the flowrate measured at the last request in ...
+                Text("Sampling Rate(mins/sample):")
+                Text(String(controller.minutesPerSample))
+                
+            }
+
+            HStack {
+                
+                // MARK: Update Flowrate
+                // Both the textfield and update button for setting a new flowrate
+                TextField("", text: $controller.minutesPerSample)
+                Button ("Set Flowrate"){
+                    
+                    // TODO: alert user for invalid inputs
+                    if var sampRate = Double(rate) {
+                        if sampRate < 0 {
+                            sampRate = 0
+                        }
+                        if sampRate > 1 {
+                            sampRate = 0
+                        }
+                        
+                        
+                        
+                        //sampling rate code for setting correct val
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+  /*
     var body: some View {
         HStack {
             Text("Minutes/Sample: ")
             TextField("Minutes / Sample", text: $controller.minutesPerSample)
         }
     }
+    
+*/
+    
 }
---
+++ FurnaceController/Tests.swift
@@ -0,0 +1,8 @@
+//
+//  Tests.swift
+//  FurnaceController
+//
+//  Created by Preston Grant Yates on 6/7/22.
+//
+
+import Foundation
