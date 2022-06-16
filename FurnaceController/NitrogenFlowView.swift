50a093ef535e5eccec1585ebf373e30f37accf66
Nathan Bujarski <nbujarski@mines.edu>
2022-06-15 23:27:54 -0600

    Updated documentation for all views

--- FurnaceController/NitrogenFlowView.swift
+++ FurnaceController/NitrogenFlowView.swift
@@ -1,16 +1,21 @@
-//
-//  NitrogenFlowView.swift
-//  FurnaceController
-//
-//  Created by Mines Student on 6/6/22.
-//
 
 import SwiftUI
 
+/// Subview containing the display of the current Nitrogen flowrate, the textfield for updating the Nitrogen flowrate, and the button for confirming an update.
+///
+/// The Nitrogen flowrate which is displayed in the text following the "Nitrogen Flowrate (L/min)" label exists within ArduinoController.
+///
+/// The textfield and button initiate the sending of a command to the Arduino, through the ArduinoController, which will update the Nitrogen flow meter's flowrate according to the value entered by the user. If the value entered is less than the minimum flowrate, it will be rounded up to the minimum flowrate. If the value entered is greater than the maximum flowrate, it will be rounded down to the maximum flowrate. The textfield and button are disabled if the connection status of the application with the Arduino and sensors is bad.
 struct NitrogenFlowView: View {
     
+    /// A string that appears within the textfield
+    ///
+    /// I am not entirely sure how the Property Wrappers of this variable work.
     @State private var flow: String = ""         // last measured flowrate
     
+    /// Reference to the ArduinoController object, arduino, that exists in AppController.
+    ///
+    /// The ArduinoController contains the last measured Nitrogen flowrate data which is displayed in this subview. It also contains the functionality for sending commands to the Arduino which update the Nitrogen flowrate.
     @ObservedObject var controller: ArduinoController = AppController.shared.arduino
     
     var body: some View {
